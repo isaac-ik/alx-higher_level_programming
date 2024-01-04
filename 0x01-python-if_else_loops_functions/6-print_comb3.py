66cd107ef15a387e238f2c2ba653ea4a8c1e44c5
isaac-ik <isaac.ikpemi@gmail.com>
2024-01-04 09:00:41 -0800

    Number combinations

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+for firstDigit in range(0, 10):
+    for secondDigit in range(0, 10):
+        if firstDigit > secondDigit:
+            pass
+        elif firstDigit == secondDigit:
+            pass
+        else:
+            if firstDigit < 8 and secondDigit < 10:
+                print("{}{}".format(firstDigit, secondDigit), end=", ")
+            else:
+                print("{}{}".format(firstDigit, secondDigit), end="\n")
+
+
+
+
+
+
