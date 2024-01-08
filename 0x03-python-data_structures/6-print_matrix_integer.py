0d1120070bcb88b7c09cae04d996921908e45db9
isaac-ik <isaac.ikpemi@gmail.com>
2024-01-08 09:05:38 -0800

    correction to algo 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in range(0, len(row)):
             if col != len(row)-1:
-                print("{:d} ".format(row[col]), end="")
+                print("{:d}".format(row[col]), end=" ")
             else:
                 print("{:d}".format(row[col]))
